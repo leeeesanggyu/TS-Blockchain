0ccae63deabaccb9d92897a44fa8e9f8ff88375b
skkkk98 <salgu1998@gmail.com>
2021-01-24 01:28:18 +0900

    argument ? 쓰는법

--- TYPE_CHAIN/index.js
+++ TYPE_CHAIN/index.js
@@ -1,1 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Leesangkyu", age = 24, gender = "male";
+const sayHI = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age} and gender is ${gender}.`);
+};
+sayHI(name, age);
 //# sourceMappingURL=index.js.map
--- TYPE_CHAIN/index.ts
+++ TYPE_CHAIN/index.ts
@@ -1,1 +1,11 @@
-alert("hello");
+const name = "Leesangkyu",
+age = 24,
+gender= "male";
+
+const sayHI = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age} and gender is ${gender}.`);
+}
+
+sayHI(name, age);
+
+export{};
